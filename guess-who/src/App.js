f21a7d2fecb234d3fcb6b3cf4192142ba5d723d2
Clark Williams <theclarkwilliams@gmail.com>
2019-07-30 11:34:53 -0600

    waiting for components to me merged

--- guess-who/src/App.js
+++ guess-who/src/App.js
@@ -14,15 +14,24 @@ function App() {
   return (
     <div className="App">
       Guess Who
-      <Link to="/">Login</Link>
-      <Link to="/signup">Sign Up</Link>
-      <Link to="/dashboard">dashboard</Link>
+      <div className="tempLinks">
+        <Link to="/">Login</Link>
+        <Link to="/signup">Sign Up</Link>
+        <Link to="/dashboard">dashboard</Link>
+        <Link to="/settings">settings</Link>
+        <Link to="/game">game</Link>
+      </div>
+
       <Route exact path="/" component={Login} />
+      {/* <Route path="/signup" component={Signup} /> */}
       <Route path="/signup" component={Signup} />
-      {/* <Route path="/dashboard" component={Dashboard} /> */}
-      <PrivateRoute path="/dashboard" component={Dashboard} />
-      <PrivateRoute path="/settings" component={Settings} />
-      <PrivateRoute path="/game" component={Game} />
+
+      <Route path="/dashboard" component={Dashboard} />
+      {/* <PrivateRoute path="/dashboard" component={Dashboard} /> */}
+      <Route path="/settings" component={Settings} />
+      {/* <PrivateRoute path="/settings" component={Settings} /> */}
+      <Route path="/game" component={Game} />
+      {/* <PrivateRoute path="/game" component={Game} /> */}
 
     </div>
   );
