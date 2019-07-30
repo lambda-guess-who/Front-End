c0622035cce4b7f1aad246ef978bc7eb7a5a1f4d
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-07-29 20:47:02 -0400

    Finished Footer, still need to add functionality to buttons

--- guess-who/src/scenes/Dashboard/Footer.js
+++ guess-who/src/scenes/Dashboard/Footer.js
@@ -5,6 +5,9 @@ function Footer(){
     return (
         <div className='footer'>
             <img src='https://www.searchpng.com/wp-content/uploads/2019/04/Star-PNG-715x715.png' />
+            <h3>0</h3>
+            <img src='https://i.imgur.com/qlJq2e2.png' />
+            <h3>0</h3>
         </div>
     )
 };
