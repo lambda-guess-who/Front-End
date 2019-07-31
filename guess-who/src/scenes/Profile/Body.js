61986757ab3ad2ec7abdc483a5a5ca6a64f748e5
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-07-31 13:05:14 -0400

    Made slight style changes

--- guess-who/src/scenes/Profile/Body.js
+++ guess-who/src/scenes/Profile/Body.js
@@ -6,7 +6,7 @@ import './styles.scss';
 function Body(){
     return (
         <div className='body'>
-            <a><img src='https://i.imgur.com/AWjw3jH.png' alt='' /></a>
+            <img src='https://i.imgur.com/AWjw3jH.png' alt='' />
             <h2>userName</h2>
             <p>@userTwitterHandle</p>
             <h3>0</h3>
@@ -15,7 +15,7 @@ function Body(){
             <img src='https://i.imgur.com/KDnz4zT.png' alt='' />
             Invite Friends
             </button>
-            <p>Instanstly unlock 10 new levels when you invite a friend</p>
+            <p>Instantly unlock 10 new levels when you invite a friend</p>
         </div>
     )
 };
