56508c36f9a6ea2fdfbc46be77cc689cafa6fcb4
Kenna <kennaalastair@gmail.com>
2019-08-01 14:26:53 -0700

    temp changes, not keeping. will fix the merge conflicts and keep new changes and forget these

--- guess-who/src/scenes/Game/Game.js
+++ guess-who/src/scenes/Game/Game.js
@@ -1,90 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 
-import { getTwitter, postScore, setNewHighScore } from '../../actions';
+import { getTwitter } from '../../actions';
 
-import './Game.scss';
+import './Game.css';
 import Tweet from './Tweet/Tweet.js';
 import Tweeters from './Tweeters/Tweeters.js'
-import TryAgain from './TryAgain';
 
 const Game = props => { 
 
     const [score, setScore] = useState(0);
     const [userAnswer, setUserAnswer] = useState('');
-    const [canAnswer, setCanAnswer] = useState(true);
 
     const fetchTwitter = () => {
         props.getTwitter();
-        setCanAnswer(true);
-
     }
     useEffect(() => {
-        fetchTwitter();
-    }, [])
+        fetchTwitter()
+    }, [props.tweet])
 
-    // const sendScore = (id, endScore) => {
-    //     props.postScore( id, endScore);
-    // }
-    const checkAnswer = (uAnswer, cAnswer) => {
-        if(canAnswer) {   
-        if(uAnswer === cAnswer) {
-            setScore(score + 1);
-        } else {
-            localStorage.setItem("prevTweet", JSON.stringify(props.tweet))       
-            localStorage.setItem("prevUserObj", JSON.stringify(props.correctUserObject))       
-            if(score > props.highScore) {
-                props.setNewHighScore(score)
-                props.postScore(props.userId, props.highScore)
-                setScore(0);
-                props.history.push("/tryagain")
-            } else {
-                setScore(0);
-                props.history.push("/tryagain")
-            }
-        }
-        setCanAnswer(false)
-        setUserAnswer('');
+    const checkAnswer = (userAnswer, correctAnswer) => {
+        if(userAnswer === correctAnswer) {
+            console.log("something");
         }
     }
 
-    const pickAnswer = tweeter => {
-        setUserAnswer(tweeter.handle)
-    }
+    const pickAnswer = () => {
 
-
-    if(props.location.pathname === "/tryagain") {
-        return <TryAgain 
-            tweet={props.tweet}
-            userObject={props.correctUserObject}
-        /> 
     }
 
     return (
         <div className="wrapper">
             <div className="game-container">
-                <Link to="/dashboard">
-                    <img src='https://i.imgur.com/liO8kXc.png' className="dashboard-icon" alt =''/>
-                </Link>
                 <div className="category-title">
                     <h1>Category</h1>
                     <p className="category">Presidential Candidates</p>
                 </div>
-                <Tweet tweet={props.tweet} userObject={props.correctUserObject} noImage={true} />
-                <Tweeters
-                    tweeters={props.tweeters}
-                    pickAnswer={pickAnswer}
-                    userAnswer={userAnswer}
-                />
+                <Tweet />
+                <Tweeters />
                 <div className="twitter-btn">
-                    {canAnswer
-                        ? <button onClick={() => checkAnswer(userAnswer, props.correctAnswer)}>Check Answer</button>
-                        : <button onClick={() => fetchTwitter()}>Next Tweet</button>
-                    }
+                    <button onClick={() => checkAnswer()}>Check answer</button>
                 </div>
-                <p>Current Score: {score}</p>
-                <p>High Score: {props.highScore}</p>
+                <p>{score}</p>
             </div>
         </div>
     )
@@ -95,15 +52,11 @@ const mapStateToProps = state => {
         ...state,
         tweet: state.tweet,
         tweeters: state.tweeters,
-        correctAnswer: state.answer.screen_name,
-        correctUserObject: state.answer,
-        highScore: state.highScore,
-        username: state.username,
-        userId: state.userId
+        correctAnswer: state.answer
     }
 }
 
 export default connect(
     mapStateToProps,
-    { getTwitter, postScore, setNewHighScore }
+    { getTwitter }
 )(Game);
--- guess-who/src/scenes/Game/Tweeters/Tweeters.js
+++ guess-who/src/scenes/Game/Tweeters/Tweeters.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Tweeters = props => {
     return (
@@ -8,13 +8,9 @@ const Tweeters = props => {
                 Who Tweeted It?!
             </p>
             <div className="tweeters">
-                {props.tweeters.map((eachTweeter, i) => {
+                {props.tweeters.map(eachTweeter => {
                     return (
-                        <div key={i}
-                            className={props.userAnswer===eachTweeter.handle ? "tweeter selected" : "tweeter"}
-                            onClick={() => {
-                                props.pickAnswer(eachTweeter)
-                            }}>
+                        <div className="tweeter" onClick={() => props.pickAnswer(eachTweeter)}>
                             <img src={eachTweeter.picture} className="profile-photo" alt={eachTweeter.name} />
                             <div className="tweeter-info">
                                 <p className="tweet-name">{eachTweeter.name}</p>
